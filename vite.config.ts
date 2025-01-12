1fa5ba801336b027f6022c1b895e6c2c47f1574a
Minsoek96 <FE.minseok96@gmail.com>
2025-01-12 23:34:17 +0900

    refact : babel runtime: ‘automatic’

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'classic',
       babel: {
         babelrc: true,
         configFile: true,
@@ -13,5 +12,10 @@ export default defineConfig({
   ],
   esbuild: {
     jsx: "preserve",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
 });
