e738d3dd9dfd18a1cfa1210f55c0e32a678024e9
Minsoek96 <FE.minseok96@gmail.com>
2025-01-13 21:06:48 +0900

    refact : virtual dom object

--- src/Test.tsx
+++ src/Test.tsx
@@ -4,8 +4,16 @@ import { createElement } from "./libs/jsx";
 export const Element = () => {
   return (
     <div className="test">
-      <h1>Hello Babel Test</h1>
+      <h1>
+        Hello Babel Test
+        <p>[]</p>
+      </h1>
       <p>transformation</p>
+      <p>
+        <div>
+          <div>Depth</div>
+        </div>
+      </p>
     </div>
   );
 };
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,6 @@
 /* eslint-disable no-console */
-import { createElement } from "./libs/jsx";
 import { Element } from "./Test";
 
-const A = () => createElement("div", null, "first");
 
-const B = Element();
-
-const Test = createElement(
-  "div",
-  { className: "test" },
-  createElement(A, null),
-  createElement("div", null, createElement("h1", null, "Hello Babel Test")),
-  createElement("p", null, "transformation"),
-);
-
-const element = Test;
-
+const element = Element();
 console.log(JSON.stringify(element, null, 2));
-console.log(B);
