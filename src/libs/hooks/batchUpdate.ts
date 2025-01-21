0d1e2e2e279f120bcea43d5e5f0415551435ff93
Minsoek96 <FE.minseok96@gmail.com>
2025-01-22 03:23:22 +0900

    feat : batchUpdate

---
+++ src/libs/hooks/batchUpdate.ts
@@ -0,0 +1,24 @@
+import { resetStore } from "@/libs/hooks";
+
+let isUpdating = false;
+const updateQueue = new Set<() => void>();
+
+/**
+ * batchUpdate
+ *
+ * 여러 상태 업데이트를 일괄적으로 처리하는 함수
+ * @param update - 실행할 업데이트 함수
+ */
+export function batchUpdate(update: () => void) {
+  updateQueue.add(update);
+  if (!isUpdating) {
+    isUpdating = true;
+
+    Promise.resolve().then(() => {
+      resetStore();
+      updateQueue.forEach((update) => update());
+      updateQueue.clear();
+      isUpdating = false;
+    });
+  }
+}
