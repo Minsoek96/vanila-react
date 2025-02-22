6613850b1a05432b28f7b44d238879306955e54a
Minsoek96 <FE.minseok96@gmail.com>
2025-02-22 23:52:26 +0900

    feat : custom router hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useRouter } from "@/libs/router";
+
 export const App = () => {
+  const router = useRouter();
+  const handleReplace = (to: string) => {
+    router.replace(to);
+  };
+
+  const handlePush = (to: string) => {
+    router.push(to);
+  };
   return (
     <div>
       <div>{false}</div>
       <div>{null}</div>
       <div>{undefined}</div>
       <div>{0}</div>
+      <div>{router.pathname}</div>
+      <button onClick={() => handleReplace("todo")}>Todo 페이지 이동</button>
+      <button onClick={() => handlePush("test")}>Test 페이지 이동</button>
+      <div>뒤로가기 방지 Test 이동</div>
       {"Vanilla React Test"}
     </div>
   );
--- src/libs/router/index.tsx
+++ src/libs/router/index.tsx
@@ -1,9 +1,40 @@
 import { PropsWithChildren } from "@/libs/types";
 
-const Router = {
-  navigate(to: string) {
-    window.history.pushState({}, "", `/${to}`);
-  },
+type RouterType = {
+  push: (to:string) => void;
+  replace: (to:string) => void;
+  forward: () => void;
+  back: () => void;
+  pathname: () => string;
+}
+
+/** 라우팅 관련 메서드들을 제공하는 커스텀 훅 */
+const useRouter = ():RouterType => {
+  const push = (to: string) => {
+    window.history.pushState({}, "", `${to}`);
+    window.dispatchEvent(new CustomEvent('routechange'));
+  }
+
+  const replace = (to:string) => {
+    window.history.replaceState({}, "", `${to}`);
+    window.dispatchEvent(new CustomEvent('routechange'));
+  }  
+
+  const forward = () => {
+    window.history.forward();
+  } 
+
+  const back = () => {
+    window.history.back();
+  }
+
+  const pathname = () => {
+    return window.location.pathname;
+  }
+
+  return {
+    push, replace, forward, back, pathname
+  }
 };
 
 type LinkProps = {
@@ -14,12 +45,21 @@ type LinkProps = {
  * SPA 라우팅 방식의 링크
  * @param to - path 경로
  */
-function Link({  children,to, ...props }: PropsWithChildren<LinkProps>) {
+function Link({ children, to, ...props }: PropsWithChildren<LinkProps>) {
   const handleClick = (e: MouseEvent) => {
     e.preventDefault();
-    window.history.pushState({}, '', to);
+
+    if (window.location.pathname === to) {
+      console.log("Prevented duplicate route:", {
+        currentPath: window.location.pathname,
+        attemptedPath: to,
+        historyLength: window.history.length,
+      });
+      return;
+    }
+    window.history.pushState({}, "", to);
     //경로 변경을 전파
-    window.dispatchEvent(new CustomEvent('routechange'));
+    window.dispatchEvent(new CustomEvent("routechange"));
   };
 
   return (
@@ -29,6 +69,4 @@ function Link({  children,to, ...props }: PropsWithChildren<LinkProps>) {
   );
 }
 
-export {
-  Router, Link
-}
+export { useRouter, Link };
